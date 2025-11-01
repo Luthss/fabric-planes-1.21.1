8d408af52c41a3a9d0eb2eab85efb77921b2fa2a
Luthss <ismailloothmohamed@gmail.com>
2025-11-01 16:06:13 +0500

    Initial commit

---
+++ src/main/java/com/luthster/planes/planesClient.java
@@ -0,0 +1,9 @@
+package com.luthster.planes;
+import net.fabricmc.api.ClientModInitializer;
+
+public class planesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
